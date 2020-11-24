1007edabf017f484d5fbd7e899688a15da8ffe37
JakeMaz <jakemazanowski@gmail.com>
2020-11-24 16:24:22 -0500

    post.../logs/create log endpoint created and tested in postman. post saving to postgress. app.js updated so that /createlog endpoint workes. deleted middleware/index.js added controllers export to index.js and same with log model.

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const db = require('./db')
 
-// app.use(express.static(__dirname + '/public'));
-// console.log(__dirname)
-app.get('/', (req, res)=> res.send('hi guys'));
+const controllers = require('./controllers')
 
-app.listen(process.env.PORT, ()=>console.log(`[Server:] App is listening on Port ${process.env.PORT}`))
+app.use(express.json());
+
+app.use('/logs', controllers.logscontroller)
+
+db.authenticate()
+    .then(() => db.sync())
+    .then(() => {
+        app.listen(process.env.PORT, () => console.log(`[Server:] App is listening on Port ${process.env.PORT}`))
+    })
+    .catch((err) => {
+        console.log("[Server:] Server Crashed");
+        console.error(err);
+    })
---
+++ controllers/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    logscontroller: require('./logscontroller')
+}

f6825be926ad7c614ef33b391b54ea9fab5e8fdc
JakeMaz <jakemazanowski@gmail.com>
2020-11-24 11:58:27 -0500

    test works

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
-console.log(__dirname)
-app.get('/', (req, res)=> res.render('index'));
+// app.use(express.static(__dirname + '/public'));
+// console.log(__dirname)
+app.get('/', (req, res)=> res.send('hi guys'));
 
 app.listen(process.env.PORT, ()=>console.log(`[Server:] App is listening on Port ${process.env.PORT}`))
